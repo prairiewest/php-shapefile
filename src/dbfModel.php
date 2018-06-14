eaa45f9acbd7f0f853a79410699916cd2a6199d3
Mikhail Nikula <mikhail.nikula@quartsoft.com>
2018-06-14 13:07:07 +0300

    Added dbf model for work with dbf file. Added refactoring for ShapeFile class

---
+++ src/dbfModel.php
@@ -0,0 +1,21 @@
+<?php
+
+class DbfModel
+{
+    public $schema = [];
+
+
+    public function __construct()
+    {
+        $dbf_schema = [["id","N",4,0]];
+
+        if(empty($this->schema)){
+            $this->schema = $dbf_schema;
+        }
+    }
+
+    public function getSchema()
+    {
+        return $this->schema;
+    }
+}
